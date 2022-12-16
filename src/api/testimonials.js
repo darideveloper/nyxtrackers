f8e066cd4fa0ab2207873d4cb653e6b6dffc2eba
darideveloper <darideveloper@gmail.com>
2022-12-16 17:59:54 -0600

    testimonials: update data

--- src/api/testimonials.js
+++ src/api/testimonials.js
@@ -1,37 +1,27 @@
 export const testimonials_data = [
     {
         image: 'user_1.png',
-        user: "John Doe 1",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Vivamus ornare sapien risus, at suscipit turpis rhoncus sit amet. Aliquam eget iaculis quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        user: "Reiven",
+        text: 'Had some difficulties setting them up at first as im not very tech savvy, but Nyx made it easy to understand and set up!',
     },
     {
         image: 'user_2.png',
-        user: "John Doe 2",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Vivamus ornare sapien risus, at suscipit turpis rhoncus sit amet. Aliquam eget iaculis quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit orci mauris, ut scelerisque nisi tristique vitae.',
+        user: "ShadowEssence",
+        text: 'Everything worked great, trackers came even faster than the amazon straps I ordered!',
     },
     {
         image: 'user_3.png',
-        user: "John Doe 3",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Vivamus ornare sapien risus, at suscipit turpis rhoncus sit amet.',
+        user: "Marx",
+        text: 'Texto de ejemplo jhdwsf dfhbjsdfjhsdfasdfh safdjhsfad sdfjhk',
     },
     {
         image: 'user_4.png',
-        user: "John Doe 4",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Vivamus ornare sapien risus, at suscipit turpis rhoncus sit amet. Aliquam eget iaculis quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit orci mauris, ut scelerisque nisi tristique vitae.',
+        user: "Sphovi",
+        text: 'I can twerk now, but for real, they work great!',
     },
     {
         image: 'user_5.png',
-        user: "John Doe 5",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales.',
-    },
-    {
-        image: 'user_6.png',
-        user: "John Doe 6",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Vivamus ornare sapien risus, at suscipit turpis rhoncus sit amet. Aliquam eget iaculis quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur suscipit orci mauris',
-    },
-    {
-        image: 'user_8.png',
-        user: "John Doe 7",
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium egestas diam et sodales. Curabitur suscipit orci mauris, ut scelerisque nisi tristique vitae.',
+        user: "John Doe 1",
+        text: 'Texto de ejemplo jhdwsf dfhbjsdfjhsdfasdfh safdjhsfad sdfjhk',
     },
 ]
