d028e5f6adc9f63e4b59824f42aa68b1923993d8
darideveloper <darideveloper@gmail.com>
2022-12-16 16:05:47 -0600

    why us: update content

--- src/api/why_us.js
+++ src/api/why_us.js
@@ -1,5 +1,17 @@
 export const why_us_data = [
-    {icon: 'why_us_icon_1.svg', title: 'Quality', text: 'We use only the best materials and components to ensure the best quality possible.'},
-    {icon: 'why_us_icon_2.svg', title: 'Reliability', text: 'We have a 100% success rate in delivering our products on time.'},
-    {icon: 'why_us_icon_3.svg', title: 'Support', text: 'Our support team is always available, day and night. No matter what you need.'},
+    {
+        icon: 'why_us_icon_1.svg',
+        title: 'Quality',
+        text: 'Nyxtrackers are built with the outmost quality in mind, personally made and individually tested ingame to ensure that the build quality is the best that you can get.',
+    },
+    {
+        icon: 'why_us_icon_2.svg',
+        title: 'Customizable',
+        text: 'From the colors to custom text/logos on the lids, Nyxtrackers are designed to be customizable and fun, with no extra charge you will get your own and unique set of trackers.',
+    },
+    {
+        icon: 'why_us_icon_3.svg',
+        title: 'Fast Delivery',
+        text: 'Nyxtrackers manufacturing process makes it possible to ship your product in 2-3 days after making the order, showing every step of the process to the customer.',
+    },
 ]
