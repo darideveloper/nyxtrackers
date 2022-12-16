f3de3c67eefeaa23bcb9f7bdf88806d719b3690d
darideveloper <darideveloper@gmail.com>
2022-12-16 17:57:30 -0600

    testimonials: responsive

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
                 <About />
                 <VideosGrid />
                 <Models />
-                {/* <Testimonials /> */}
+                <Testimonials />
                 {/* <WhyUs /> */}
             </main>
             {/* <Footer /> */}
--- src/sections/testimonials.jsx
+++ src/sections/testimonials.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react'
 import { testimonials_data } from '../api/testimonials'
-import { wrap_text } from '../tools/text'
 import SplideGallery from '../components/splide_gallery'
 import TitleSeparator from '../components/title_separator'
 
 export default function Testimonials() {
 
-    // Truncate the number of characters in the testimonial text
-    testimonials_data.forEach((testimonial) => {
-        testimonial.text = wrap_text(testimonial.text, 250)
-    })
+    const [perPage, setPerPage] = useState(3)
+
+    function handleResize() {
+        // Resize gallery elements
+
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            setPerPage (1)
+        } else if (window.matchMedia("(max-width: 1000px)").matches) {
+            setPerPage (2)
+        } else if (window.matchMedia("(max-width: 1250px)").matches) {
+            setPerPage (3)
+        }
+    }
+
+    // Resize gallery elements when screen size changes
+    addEventListener("resize", (event) => {
+        handleResize ()
+    });
+
+    // Resize gallery elements when page loads
+    useEffect(() => {
+        handleResize ()
+    }, [])
 
     return (
         <section
@@ -20,7 +39,7 @@ export default function Testimonials() {
             <SplideGallery
                 cards_data={testimonials_data}
                 options={{
-                    perPage: 3,
+                    perPage: perPage,
                     rewind: true,
                     padding: '5rem',
                     perMove: 1,
