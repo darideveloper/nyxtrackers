8576f198a1c5075c2308d5f021f4a639b50a23a2
darideveloper <darideveloper@gmail.com>
2023-02-27 00:07:30 -0600

    feat (alert) alert after load

---
+++ src/api/alert.js
@@ -0,0 +1,2 @@
+export const alert_title = 'New tracker Design!'
+export const alert_text = "Check the customizations! Join the discord for ordering!"
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './css/styles.css'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { alert_title, alert_text } from './api/alert'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+// Ahos alert after loading ends
+const MySwal = withReactContent(Swal)
+setTimeout(() => {
+  MySwal.fire({
+    position: 'top-end',
+    icon: 'info',
+    title: alert_title,
+    text: alert_text,
+    showConfirmButton: false,
+    timer: 8000,
+  })
+}, 4000)
