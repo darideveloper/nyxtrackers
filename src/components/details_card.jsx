4ecfb434b959a2395f919742270dee876d2936eb
darideveloper <darideveloper@gmail.com>
2023-05-25 18:35:48 -0600

    feat (about) update about data

--- src/components/details_card.jsx
+++ src/components/details_card.jsx
@@ -2,25 +2,29 @@ import PropTypes from 'prop-types'
 import Video from './video'
 import Chibis from './chibis'
 
-export default function DetailsCard ({ title, video, texts }) {
-    return (
-        <div className='details-card card'>
-            <h3>{title}</h3>
-            <div className="content">
-                <Video video={video} />
-                <div className="texts">
-                    {texts.map ((text, index) => (
-                        <p key={index}>{text}</p>
-                    ))}
-                </div>
-                <Chibis />
-            </div>
-        </div>
-    )
+export default function DetailsCard({ title, video, texts }) {
+  return (
+    <div className='details-card card'>
+      <h3>{title}</h3>
+      <div className="content">
+        <Video video={video} />
+        <p className="texts">
+          {texts.map((text, index) => (
+            <span key={index}>{text}</span>
+          ))}
+          <div className="white-spaces">
+            <br />
+            <br />
+          </div>
+        </p>
+        <Chibis />
+      </div>
+    </div>
+  )
 }
 
 DetailsCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    video: PropTypes.string.isRequired,
-    texts: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  texts: PropTypes.array.isRequired,
 }
