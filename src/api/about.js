8a1ac95a45ee138c332223bece818f60b5733476
darideveloper <darideveloper@gmail.com>
2023-05-28 13:51:26 -0600

    chore (api/about) update data

--- src/api/about.js
+++ src/api/about.js
@@ -1,38 +1,31 @@
 export const about_cards_data = [
-    {
-        title: 'Affordable Full body tracking solution',
-        video: 'about_video_1.mp4',
-        texts: [
-            'Nyxtrackers are a custom made Slimevr trackers, they offer the complete full body tracking capabilitieswith a really affordable and competitive price',
-            'The tracking is surprisingly good and drift time has increased from 10-15 min to 25-30 min.',
-            'Nyxtrackers are customizable, making them personalized to you, from colors to logos!'
-        ],
-    },
-    {
-        title: 'No BaseStations needed',
-        video: 'about_video_2.mp4',
-        texts: [
-            'Nyxtrackers represent a bespoke solution for Slimevr trackers, providing comprehensive full body tracking capabilities at an impressively competitive price point.',
-            'The superior performance of these trackers is noteworthy, with tracking accuracy that surpasses expectations. Furthermore, drift time has seen a significant improvement, expanding from the previous range of 10-15 minutes to an enhanced 25-30 minutes', 
-            'In terms of personalization, Nyxtrackers are exceptionally versatile. They offer a wide range of customization options, from individual color preferences to the inclusion of personal logos.',
-            'This ensures that each Nyxtracker can be tailored specifically to the user, enhancing the individuality and personal connection to the device.',
-        ],
-    },
-    {
-        title: 'For every VR headset',
-        video: 'about_video_3.mp4',
-        texts: [
-          'Nyxtrackers dont need basestations, they connect directly to wifi and work creating a virtual representation of your bone structure, using the gyroscopes and accelerometers in the trackers.',
-          'With this, The trackers work even in tight spaces, aswell as below your clothes/ covers!',
-        ],
-    },
-    {
-        title: 'Standalone Quest Compatible',
-        video: 'about_video_4.mp4',
-        texts: [
-          'NyxTrackers are not dependent on base stations; rather, they employ direct wireless fidelity (Wi-Fi) connectivity to function',
-          `Their principal operation revolves around constructing a virtual simulation of the user's skeletal framework, facilitated by the integration of gyroscopes and accelerometers in the tracking devices`,
+  {
+      title: 'Affordable Full body tracking solution',
+      video: 'about_video_1.mp4',
+      texts: [
+          'Nyxtrackers represent a bespoke solution for Slimevr trackers, providing comprehensive full body tracking capabilities at an impressively competitive price point.',
+          'The superior performance of these trackers is noteworthy, with tracking accuracy that surpasses expectations. Furthermore, drift time has seen a significant improvement, expanding from the previous range of 10-15 minutes to an enhanced 25-30 minutes.',
+          'In terms of personalization, Nyxtrackers are exceptionally versatile. They offer a wide range of customization options, from individual color preferences to the inclusion of personal logos.',
+          'This ensures that each Nyxtracker can be tailored specifically to the user, enhancing the individuality and personal connection to the device.',
+      ],
+  },
+  {
+      title: 'No BaseStations needed',
+      video: 'about_video_2.mp4',
+      texts: [
+          'NyxTrackers are not dependent on base stations; rather, they employ direct wireless fidelity (Wi-Fi) connectivity to function.',
+          "Their principal operation revolves around constructing a virtual simulation of the user's skeletal framework, facilitated by the integration of gyroscopes and accelerometers in the tracking devices.",
           'Due to this sophisticated design, these trackers can proficiently function in confined spaces. Furthermore, they maintain effective performance even when positioned under garments or covers.',
-        ],
-    },
+      ],
+  },
+  {
+      title: 'For every VR headset',
+      video: 'about_video_3.mp4',
+      texts: ['Nyxtrackers simulate Vive trackers in SteamVR, making them compatible with every headset out in the market that connects to SteamVR!'],
+  },
+  {
+      title: 'Standalone Quest Compatible',
+      video: 'about_video_4.mp4',
+      texts: ['Nyxtrackers are compatible with OSC standalone quest FBT meaning now you dont even need to use PCVR to connect your trackers!'],
+  },
 ]
