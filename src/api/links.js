1e5ac6a7dca9c7ffcb8a170d1f66ca4de7582542
daridev <darideveloper@gmail.com>
2023-07-23 14:15:19 -0600

    feat (api/links): kofi as first link

--- src/api/links.js
+++ src/api/links.js
@@ -1,32 +1,32 @@
 export const payments_data = [
-    {
-        image: 'payment_1.svg',
-        name: 'Visa',
-    },
-    {
-        image: 'payment_2.svg',
-        name: 'Mastercard',
-    },
-    {
-        image: 'payment_3.svg',
-        name: 'Paypal',
-    },
-    {
-      image: 'kofi-logo.png',
-      name: 'Kofi',
+  {
+    image: 'payment_1.svg',
+    name: 'Visa',
+  },
+  {
+    image: 'payment_2.svg',
+    name: 'Mastercard',
+  },
+  {
+    image: 'payment_3.svg',
+    name: 'Paypal',
+  },
+  {
+    image: 'kofi-logo.png',
+    name: 'Kofi',
   },
 ]
 
 export const links_socials_data = [
-    {
-        title: 'Discord',
-        link: 'https://discord.com/invite/YSYyDpeF8R',
-        image: 'discord-logo.svg',
-    },
-    {
-      title: 'Ko-Fi',
-      link: 'https://ko-fi.com/nyxtrackers',
-      image: 'kofi-logo.png',
+  {
+    title: 'Ko-Fi',
+    link: 'https://ko-fi.com/nyxtrackers',
+    image: 'kofi-logo.png',
+  },
+  {
+    title: 'Discord',
+    link: 'https://discord.com/invite/YSYyDpeF8R',
+    image: 'discord-logo.svg',
   },
 ]
 
