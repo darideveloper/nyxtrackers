1daad9e424b7e9d7b3d713e35ed5a5fe35f4f4b9
daridev <darideveloper@gmail.com>
2023-08-18 15:59:02 -0600

    feat (form): send email to api

--- src/api/form.js
+++
@@ -1,2 +0,0 @@
-const form_api = import.meta.VITE_VITE_FROM_API
-console.log (form_api)
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,8 +1,58 @@
 import { useState } from "react"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 export default function Form () {
 
+  const [email, setEmail] = useState('')
   const [formActive, setFormActive] = useState(false)
+
+  const formHost = import.meta.env.VITE_FORM_HOST
+  const formEndpoint = `${formHost}/contact-form`
+  const loading = document.getElementById('loading')
+
+  async function handleSubmit (e) {
+    e.preventDefault()
+
+    // Show loading
+    loading.style.display = 'flex'
+
+    // Submit form data as json
+    const headers = new Headers()
+    headers.append("Content-Type", "application/json")
+    headers.append("Accept", "application/json")
+    let response = await fetch(formEndpoint, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({"email": email}),
+    })
+    console.log (response)
+    console.log (response.status)
+    
+    // Disable loading
+    loading.style.display = 'none'
+
+    // Validate response status
+    let alertTitle = ''
+    let alertText = ''
+    if (response.status == 200) {
+      alertTitle = "Thank you for suscribing!"
+      alertText = "Now you will get notified when commissions open, by email!"
+    } else {
+      alertTitle = "Error"
+      alertText = "Please try again later"
+    }
+    
+    // Show alert
+    setTimeout(() => {
+      const MySwal = withReactContent(Swal)
+      MySwal.fire({
+        title: alertTitle,
+        text: alertText,
+        showConfirmButton: false,
+      })
+    }, 1000)
+  }
 
   return (
     <div 
@@ -10,13 +60,32 @@ export default function Form () {
       onMouseEnter={() => {setFormActive(true)}}
       onMouseLeave={() => {setFormActive(false)}}
     >
-      <form>
+      <form
+        onSubmit={handleSubmit}
+      >
         <h2>Sign up to our mailing list</h2>
         <p>You will get notified when commissions opens</p>
         <div className={`inputs`}>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="sample@email.com" required autoComplete="off"/>
-          <input type="submit" value="Suscribe" className="btn submit" />
+          <label 
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input 
+            type="email" 
+            name="email" 
+            id="email" 
+            placeholder="sample@email.com" 
+            required 
+            autoComplete="off"
+            value={email}
+            onChange={(e) => {setEmail(e.target.value)}}
+          />
+          <input 
+            type="submit" 
+            value="Suscribe" 
+            className="btn submit" 
+          />
         </div>
       </form>
     </div>
