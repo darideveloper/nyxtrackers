b93844b51d19dd0673a3d296dfdcb6bcfd45f829
daridev <darideveloper@gmail.com>
2024-07-27 08:54:19 -0600

    fix (settings): no brake hmr

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
     watch: {
-      // Add custom paths to watch for full page refreshes
-      additionalPaths: (watcher) => {
-        watcher.add('src/**');
-        watcher.add('src/sections/*.*');
-        watcher.add('src/css/*.*');
-      }
+      usePolling: true,
     }
-  }
+  },
 })
