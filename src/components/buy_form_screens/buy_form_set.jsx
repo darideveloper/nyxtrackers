5605f772fa33c0c7b277563a6e316e03000a0691
daridev <darideveloper@gmail.com>
2024-07-31 10:17:38 -0600

    style (buy_form/set): responsive

--- src/components/buy_form_screens/buy_form_set.jsx
+++ src/components/buy_form_screens/buy_form_set.jsx
@@ -2,8 +2,6 @@ import { setsOptions, colorsOptions } from '../../api/buy_form'
 
 export default function BuyFormSet() {
 
-  console.log({setsOptions})
-
   return (
     <section
       className="set"
@@ -16,14 +14,16 @@ export default function BuyFormSet() {
             role="button"
           >
             <img src={`/sets/${set.name} ${colorsOptions[0]}.webp`} />
-            <h3>{set.name}</h3>
-            <p>{set.points} points</p>
-            <p>${set.price}</p>
-            {
-              set.recommended 
-              &&
-              <p className='tag'>Recommended</p>
-            }
+            <div className="info">
+              <h3>{set.name}</h3>
+              <p>{set.points} points</p>
+              <p>${set.price}</p>
+              {
+                set.recommended
+                &&
+                <p className='tag'>Recommended</p>
+              }
+            </div>
           </div>
         ))
       }
