db740580580bf18f689c18f67fd2dd1bf62dff7a
daridev <darideveloper@gmail.com>
2024-10-10 12:49:09 -0600

    feat (buy_card): show form when click

--- src/components/buy_card.jsx
+++ src/components/buy_card.jsx
@@ -1,37 +1,50 @@
 import PropTypes from 'prop-types'
 import Button from './button'
 
-export default function BuyCard ({name, is_new, is_available, image, price, features, buy_link}) {
-    return (
-        <div className="buy-card card">
-            <header>
-                { is_new && <span className="new">New</span> }
-                { is_available && <span className="available">Available</span> }
-            </header>
-            <div className="content">
-                <img src={image} alt={`${name} photo`} className="model-image" />
-                <div className="separator"></div>
-                <div className="info">
-                    <h3>{name}</h3>
-                    <span className="price"> {price} €</span>
-                    <ul className="features">
-                        {features.map((feature, index) => (
-                            <li key={index}>{feature}</li>
-                        ))}
-                    </ul>
-                    <Button link={buy_link} text="Learn more &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Buy Now" />
-                </div>
-            </div>
+import { showForm } from '../features/buy_form_visible_slice'
+import { useDispatch } from 'react-redux'
+
+
+export default function BuyCard({ name, is_new, is_available, image, price, features, buy_link }) {
+  
+  const dispatch = useDispatch()
+
+  return (
+    <div className="buy-card card">
+      <header>
+        {is_new && <span className="new">New</span>}
+        {is_available && <span className="available">Available</span>}
+      </header>
+      <div className="content">
+        <img src={image} alt={`${name} photo`} className="model-image" />
+        <div className="separator"></div>
+        <div className="info">
+          <h3>{name}</h3>
+          <span className="price"> {price} €</span>
+          <ul className="features">
+            {features.map((feature, index) => (
+              <li key={index}>{feature}</li>
+            ))}
+          </ul>
+          <Button
+            text="Learn more &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; Buy Now"
+            onClick={() => {
+              // Show form with redux
+              dispatch(showForm())
+            }}
+          />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 BuyCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    is_new: PropTypes.bool.isRequired,
-    is_available: PropTypes.bool.isRequired,
-    image: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    features: PropTypes.array.isRequired,
-    buy_link: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  is_new: PropTypes.bool.isRequired,
+  is_available: PropTypes.bool.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  features: PropTypes.array.isRequired,
+  buy_link: PropTypes.string.isRequired,
 }
