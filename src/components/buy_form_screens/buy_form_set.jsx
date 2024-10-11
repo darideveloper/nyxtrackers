138e9bbaadb5e3eccc6b2e2e978ea504a18a0c6c
daridev <darideveloper@gmail.com>
2024-10-11 13:23:56 -0600

    feat (bu_form_set): validate if use has a pending order

--- src/components/buy_form_screens/buy_form_set.jsx
+++ src/components/buy_form_screens/buy_form_set.jsx
@@ -1,4 +1,8 @@
+import { useRef } from 'react'
+
 import SetBtn from '../set_btn'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 import { setsOptions } from '../../api/buy_form'
 import { useSelector } from 'react-redux'
@@ -11,14 +15,62 @@ import { submitEvent } from '../../libs/google-analytics'
 
 export default function BuyFormSet() {
 
+  const apiBase = import.meta.env.VITE_DASHBOARD_API
+  const MySwal = withReactContent(Swal)
+
+  // Refs
+  const pendingOrdersValidated = useRef(false)
+
   // Redux  
   const dispatch = useDispatch()
   const selectedSet = useSelector(state => state.buyFormData.setSelected)
   const selectedColor = useSelector(state => state.buyFormData.colorSelected)
+  const email = useSelector(state => state.buyFormData.email)
 
   useEffect(() => {
     // Enable next screen when load
     dispatch(setHasNext(true))
+
+    if (pendingOrdersValidated.current) {
+      return
+    }
+
+    // Validate if user already have a pending order
+    const endpoint = `${apiBase}/store/pending-order/`
+    const dataJson = JSON.stringify({
+      "email": email,
+    })
+    fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: dataJson,
+    })
+    .then(response => response.json())
+    .then(json_data => {
+      if (json_data.data.has_pending_order) {
+        
+        // Show alert if user have a pending order
+        MySwal.fire({
+          "title": "You have a pending order",
+          "text": "If you continue, your pending order will be canceled. Do you want to continue?",
+          "icon": "warning",
+          "showCancelButton": true,
+          "confirmButtonText": "Create new order",
+          "cancelButtonText": "Keep my pending order",
+        })
+        .then((result) => {
+          if (result.isDismissed) {
+            // Refresh page if user wants to keep pending order
+            window.location.href = '/'
+          }
+        })
+      }
+    })
+
+    pendingOrdersValidated.current = true
+
   }, [])
 
   return (
@@ -27,7 +79,7 @@ export default function BuyFormSet() {
     >
       {
         setsOptions.map((set, index) => (
-          <SetBtn 
+          <SetBtn
             key={index}
             name={set.name}
             color={selectedColor}
