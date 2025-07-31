141dc41048df188143297ef67270e7a9294fe5f2
daridev <darideveloper@gmail.com>
2025-07-31 14:05:43 -0600

    chore (LandingPage): update texts

--- src/api/technicals.js
+++ src/api/technicals.js
@@ -1,10 +1,9 @@
 export const technicalsData = [
   "1x 3.7v 1000mAh Lipo Battery: A rechargeable LiPo battery with built-in protection circuitry.",
   "1x Esp 12F: A compact WiFi microcontroller ideal for IoT applications.",
-  "1x Bmi270 Gyroscope: A new and improved IMU for tracking device movement and orientation.",
+  "1x ICM-45686 Gyroscope: A next-generation IMU offering high-precision motion tracking with up to 60 minutes of drift-free performance and ~40% less drift than competing sensors.",
   "1x TP4057: A standalone linear Li-Ion battery charger with built-in safety features.",
   "1x Custom PCB board: The backbone of the device, connecting and facilitating communication between all components.",
   "1x Main case (3D printed in PLA+): Durable and lightweight protective housing.",
-  "1x JST-XH 4-pin Connector: This is a standard connector often used in RC hobby electronics. It's utilized here to connect auxiliary trackers.",
   "1x JST-PH Connector for Battery: A commonly used connector for attaching batteries, ensuring a secure and reliable power connection for your device.", 
 ]
